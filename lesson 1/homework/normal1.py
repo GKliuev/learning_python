6dce33286e15501ad84768ed8f34ceec5b40e2e9
GKliuev <klyuevgrigoriy@gmail.com>
2019-05-01 13:40:37 +0300

    Add second variant

--- lesson 1/homework/normal1.py
+++ lesson 1/homework/normal1.py
@@ -9,8 +9,17 @@
 while True:
     user_number = int(input('Введите число больше 0 и меньше 10: '))
     if user_number < 0 or user_number > 10:
+        print('Вы ввели неверное число')
         continue
     else:
         print(user_number ** 2)
         break
 
+
+# while True:
+#     user_number = int(input('Введите число больше 0 и меньше 10: '))
+#     if 0 < user_number < 10:
+#         print(user_number ** 2)
+#         break
+#     else:
+#         print('Вы ввели неверное число')
