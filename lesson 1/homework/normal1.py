ab6ec48a027c3358ed6fe4e181a8414d7be1fe64
GKliuev <klyuevgrigoriy@gmail.com>
2019-05-01 12:38:52 +0300

    Fix cycle

--- lesson 1/homework/normal1.py
+++ lesson 1/homework/normal1.py
@@ -9,7 +9,7 @@
 while True:
     user_number = int(input('Введите число больше 0 и меньше 10: '))
     if user_number < 0 or user_number > 10:
-        user_number = int(input('Введите число больше 0 и меньше 10: '))
+        continue
     else:
         print(user_number ** 2)
         break
