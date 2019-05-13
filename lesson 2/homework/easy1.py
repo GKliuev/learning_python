04581b7bc0cf4194d6ee0e8ea2fa1e169bf05906
GKliuev <klyuevgrigoriy@gmail.com>
2019-05-13 21:33:45 +0300

    added teacher's solution

--- lesson 2/homework/easy1.py
+++ lesson 2/homework/easy1.py
@@ -1,5 +1,13 @@
+# my solution
+# list_fruits = ["яблоко", "банан", "киви", "арбуз"]
+# i = 0
+# for fruit in list_fruits:
+#     i += 1
+#     print(str(i) + '.', fruit.rjust(10))
+
+# teacher's solution
 list_fruits = ["яблоко", "банан", "киви", "арбуз"]
-i = 0
-for fruit in list_fruits:
-    i += 1
-    print(str(i) + '.', fruit.rjust(10))
+right_offset = len(max(list_fruits, key=len))
+for index, item in enumerate(list_fruits, start=1):
+    print('{}. {}'.format(index, item.rjust(right_offset)))
+
