2caa41e786364c45b4a8ec6477e51c7aa7e8a74f
GKliuev <klyuevgrigoriy@gmail.com>
2019-05-13 22:37:37 +0300

    add teacher's solution

--- lesson 2/homework/normal3.py
+++ lesson 2/homework/normal3.py
@@ -1,15 +1,24 @@
 import random
 
+# my solution
+# def random_number_list(n, range_min, range_max):
+#     list_random = []
+#     i = 0
+#     while i < n:
+#         rand = random.randint(range_min, range_max)
+#         list_random.append(rand)
+#         i += 1
+#     return list_random
+
+# teacher's solution
 
 def random_number_list(n, range_min, range_max):
     list_random = []
-    i = 0
-    while i < n:
-        rand = random.randint(range_min, range_max)
-        list_random.append(rand)
-        i += 1
+    for _ in range (n):
+        list_random.append(random.randint(range_min, range_max))
     return list_random
 
 
-list_random = random_number_list(30, -100, 100)
+elem_count = int(input('Введите целое число: '))
+list_random = random_number_list(elem_count, -100, 100)
 print(list_random)
